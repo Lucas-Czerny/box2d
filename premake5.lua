5f5fe18622f5e0818dcd661b76126ca0edb2e6a8
Lucas-Czerny <70445181+Lucas-Czerny@users.noreply.github.com>
2021-09-24 21:04:46 +0200

    cxy vhjb

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,28 @@
+project "Box2D"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
 
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/glad/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+			
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
